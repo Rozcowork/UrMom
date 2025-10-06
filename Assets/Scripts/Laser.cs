aae3f716a6de8d935c263629303a4ee9440e3228
Rozcowork <ethanorozco258@gmail.com>
2025-10-06 15:15:38 -0700

    Laser Obstacle
    Created laser obstacle script and need to add patrol points to the Hiearchy.

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter2D(Collider2D other) //when any pbject collides with the trigger of this object 
+    {
+        if (other.CompareTag("Player")) //if that object is the Player
+        {
+            PlayerController playerRespawn = other.GetComponent<PlayerController>(); // grab the player controller script from the object
+            if (playerRespawn != null) //if the player controller script exists on that object
+            {
+                playerRespawn.Respawn(); //Respawn the player
+            }
+        }
+    }
+}
